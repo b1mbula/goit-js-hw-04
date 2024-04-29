50442f6be4d864417241e6b17bb1fbdba6965ed5
b1mbula <danja.rubeghanskij@gmail.com>
2024-04-29 21:02:18 +0200

    hqw

--- js/task-3.js
+++ js/task-3.js
@@ -2,13 +2,13 @@ const profile = {
   username: "Jacob",
   playTime: 300,
   getInfo() {
-    console.log(`${profile.username} has ${profile.playTime} active hours`);
+    return `${this.username} has ${this.playTime} active hours`;
   },
   changeUsername(newName) {
-    profile.username = newName;
+    this.username = newName;
   },
   updatePlayTime(hours) {
-    profile.playTime += hours;
+    this.playTime += hours;
   },
 };
 
